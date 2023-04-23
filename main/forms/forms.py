48d051caa24a23abc1a5ea55a94febfa33928658
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-04-23 17:07:35 -0300

    style: styled the contact us fomr

--- main/forms/forms.py
+++ main/forms/forms.py
@@ -4,10 +4,31 @@ from django import forms
 class ContactUsForm(forms.Form):
     name = forms.CharField(max_length=100,
                            required=True,
+                           label='Seu nome',
+                           widget=forms.TextInput(
+                               attrs={
+                                   'placeholder': 'Ex: João da Silva',
+                                   'class': 'C-form__input',
+                               }
+                           )
                            )
     email = forms.EmailField(max_length=255,
                              required=True,
+                             label='Email',
+                             widget=forms.EmailInput(
+                                 attrs={
+                                     'placeholder': 'email@email.com',
+                                     'class': 'C-form__input',
+                                 }
+                             )
                              )
     message = forms.CharField(max_length=255,
                               required=True,
+                              label='Mensagem',
+                              widget=forms.Textarea(
+                                  attrs={
+                                      'placeholder': 'Sua mensagem',
+                                      'class': 'C-form__textarea',
+                                  }
+                              )
                               )
