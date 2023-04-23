4042970ccae71b764fede719539e3113b54dd026
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-04-22 22:53:23 -0300

    style: created the style for help page

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,5 @@ app_name: str = 'main'
 
 urlpatterns: list = [
     path('', views.main, name='main'),
+    path('ajuda/', views.help, name='help'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 
-def main(request):
-    return render(request,
-                  'main/pages/main.html',
-                  context={
-                      'main': 'ISTO É UM TESTE.'
-                  },
-                  )
+def main(request: HttpRequest) -> HttpResponse:
+    return render(
+        request,
+        'main/pages/main.html',
+        )
+
+
+def help(request: HttpRequest) -> HttpResponse:
+    return render(
+        request,
+        'main/pages/help.html',
+    )
