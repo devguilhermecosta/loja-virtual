8b9554ce2190df57d37f3641dd00902ad65da5ed
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-04-27 21:13:47 -0300

    feat: populated the admin area with admin.ModelAmin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "slug", ]
+    list_display_links = ["id", "name", "slug", ]
+    list_filter = ["name", ]
+    prepopulated_fields = {'slug': ('name', )}
+    search_fields = ["name", ]
+    ordering = ["-id"]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "slug",
+        "price",
+        "stock",
+        "available",
+        "created_at",
+        "updated_at",
+    ]
+    list_filter = [
+        "available",
+        "created_at",
+        "updated_at",
+        ]
+    list_editable = [
+        "price",
+        "stock",
+        "available",
+    ]
+    prepopulated_fields = {'slug': ('name',)}
